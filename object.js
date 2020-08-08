b44711a3b3f70de4eb064acfa85f24564bdb32d3
ishmam ali <ishmamalikhan@gmail.com>
2020-08-08 11:23:33 +0500

    objects

---
+++ object.js
@@ -0,0 +1,20 @@
+var myQuestions =[
+    {
+        question:"what is your name",
+        answer:{
+            a: "ishmam",
+            b:"ali",
+            c:"khan"
+        },
+        correctAnswer:"c"
+    },
+    {
+        question:"what is your age",
+        answer:{
+            a: "22",
+            b:"24",
+            c:"26"
+        },
+        correctAnswer:"b"
+    },
+]
