c9454bc9efa22c88efe6bf120f632e8cae253093
Ishmam Ali <56753433+ishmamAli786@users.noreply.github.com>
2020-09-26 23:30:02 -0700

    aysnc callback and promise

---
+++ Async.js
@@ -0,0 +1,60 @@
+// console.log('start group')
+// // let data='content';
+// // setTimeout(function(){
+// //     console.log(data)
+// // },1000)
+// function breakfast(){
+//     setTimeout(function(){
+//         console.log('breakfast complete')
+//     },100)
+// }
+// function morning(){
+//     console.log('breakfast start');
+//     breakfast()
+// }
+// morning()
+// console.log('end group')
+
+
+// function printGreeting(msg,alertGreet){
+//     if(msg==='evening'){
+//         evening(alertGreet)
+//     }
+//     else if (msg===morning){
+//         morning(alertGreet)
+//     }
+//     else {
+//         night(alertGreet)
+//     }
+// }
+// function morning(alertGreet){
+//     alert('good morning')
+//     alertGreet('Hello World')
+// }
+// function evening(alertGreet){
+//     alert('good evening')
+//     alertGreet('Hello World')
+// }
+// function night(alertGreet){
+//     alert('good night')
+//     alertGreet('Hello World')
+// }
+// function alertGreet(){
+//     alert(msg)
+// }
+// printGreeting('evening',alertGreet)
+
+
+// let promise =new Promise(function(resolve,reject){
+//     resolve()
+// })
+// function checkUserPromise(userInput){
+//     new Promise(function(resolve,reject){
+//         if(userInput){
+//             resolve();
+//         }
+//         else{
+//             reject();
+//         }
+//     })
+// }
